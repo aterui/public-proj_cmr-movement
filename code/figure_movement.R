fe980c57dd56d8f6bbf57660e36abdd17544f117
AkiraTerui <hanabi0111@gmail.com>
2023-06-20 10:25:14 -0400

    edit: format codes
    corrected some potential errors

---
+++ code/figure_movement.R
@@ -0,0 +1,19 @@
+
+# Setup -------------------------------------------------------------------
+
+rm(list=ls())
+source(here::here("code/library.R"))
+
+# call "format_cmr_data.R"
+source(here::here("code/format_cmr_data.R"))
+
+
+# figure ------------------------------------------------------------------
+
+## plot movement vs. density
+
+## plot movement vs. body size
+
+## plot habitat variables vs. density
+
+## plot habitat variables vs. movement
