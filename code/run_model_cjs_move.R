545f2f219b53f6b9c843ffb3543f6d4fc99e3467
aterui <hanabi0111@gmail.com>
2024-05-07 16:52:16 -0400

    edit: change indent

--- code/run_model_cjs_move.R
+++ code/run_model_cjs_move.R
@@ -11,168 +11,168 @@ rm(list = ls())
 df_tag0 <- readRDS("data_formatted/data_cmr.rds") # all data
 
 ## separate model by species 
- usp <- c("bluehead_chub",
-          "creek_chub",
-          "green_sunfish",
-          "redbreast_sunfish") %>%
-   sort()
+usp <- c("bluehead_chub",
+         "creek_chub",
+         "green_sunfish",
+         "redbreast_sunfish") %>%
+  sort()
 
 df_con <- readRDS("data_formatted/data_move.rds") #consecutive movement df
 
 list_est_usp <- foreach(x = usp) %do% {
   df_i <- filter(df_tag0, species == x)
   df_c <- filter(df_con, species == x)
-
-df_tag <- df_i %>% #was df_tag0
-  #filter(species == sp) %>% 
-  mutate(tag_index = as.numeric(as.factor(tag_id))) %>% 
-  arrange(tag_index, occasion) %>% 
-  relocate(tag_index, occasion)
-
-## interval information
-intv0 <- df_i %>% #was df_tag0
-  group_by(occasion) %>% 
-  summarize(min_date0 = min(datetime)) %>% 
-  mutate(min_date1 = lead(min_date0),
-         intv = round(min_date1 - min_date0) %>% 
-           as.numeric()) %>% 
-  drop_na(intv) %>% 
-  pull(intv)
-
-df_consec <- df_c %>% 
-  #readRDS("data_formatted/data_move.rds") %>% 
-  #filter(species == sp) %>% 
-  mutate(intv = datetime1 - datetime0) %>% 
-  group_by(occasion0) %>% 
-  mutate(intv = ifelse(is.na(intv),
-                       yes = intv0[occasion0],
-                       no = intv),
-         intv = round(intv)) %>% 
-  select(tag_id,
-         occasion = occasion0,
-         intv) %>% 
-  ungroup()
-
-## create vectorized recapture format
-df_y <- df_tag %>% 
-  pivot_wider(id_cols = c(tag_index, tag_id),
-              names_from = occasion,
-              values_from = section) %>% 
-  pivot_longer(cols = -c(tag_index, tag_id),
-               names_to = "occasion",
-               values_to = "y") %>% 
-  mutate(y = ifelse(is.na(y), 0, 1),
-         occasion = as.numeric(occasion)) %>% 
-  arrange(tag_index, occasion) %>% 
-  left_join(df_consec)
-
-## append season column to df_y
-df_season <- df_tag %>% 
-  mutate(month = format(datetime, "%m") %>% 
-           as.numeric(month),
-         season = ifelse(between(month, 4, 9),
-                         yes = 1,
-                         no = 0)) %>% 
-  distinct(occasion,
-           season) %>% 
-  arrange(occasion)
-
-df_y <- left_join(df_y, df_season)
-
-# create first capture vector 
-df_fc <- df_y %>% 
-  group_by(tag_index) %>% 
-  filter(y == 1) %>% 
-  slice(which.min(occasion)) %>% 
-  ungroup() %>% 
-  arrange(tag_index)
-
-## create movement vector
-df_dist <- df_tag %>% 
-  pivot_wider(id_cols = tag_index,
-              names_from = occasion,
-              values_from = section) %>% 
-  pivot_longer(cols = -tag_index,
-               names_to = "occasion",
-               values_to = "section") %>% 
-  mutate(x = 10 * section - 5,
-         occasion = as.numeric(occasion)) %>% 
-  arrange(tag_index, occasion) %>% 
-  drop_na(x)
-
-## binary recapture record
-list_recap <- with(df_y,
-                   list(Y = y, # capture state  
-                        Id_tag_y = tag_index, # tag id
-                        Id_occ_y = occasion, # occasion
-                        Nind = n_distinct(tag_index), # n unique ind
-                        Nocc = n_distinct(occasion), # n unique occasions
-                        Nobs = nrow(df_y),
-                        Season = season,
-                        L = 430, # total length
-                        Fc = df_fc$occasion # first capture
-                   ))
-
-list_intv <- with(df_y %>% drop_na(intv),
-                  list(Intv = intv,
-                       Nint = nrow(df_y %>% drop_na(intv)),
-                       Id_tag_int = tag_index, # tag id
-                       Id_occ_int = occasion))
-
-## movement distance
-list_move <- with(df_dist,
-                  list(X = x, # distance class
-                       Id_tag_x = tag_index,
-                       Id_occ_x = occasion,
-                       Nx = nrow(df_dist)))
-
-
-d_jags <- c(list_recap, list_intv, list_move)
-
-para <- c("sd_x", "mean.p", "mu.p", "alpha", "mean.phi", "sd_phi")
-
-# mcmc setup --------------------------------------------------------------
-
-## model file ####
-mcjs <- runjags::read.jagsfile("code/model_cjs_move.R")
-
-## mcmc setup ####
-n_ad <- 1000
-n_iter <- 1.0E+3
-n_thin <- max(3, ceiling(n_iter / 250)) #happens second want chains to converge 
-n_burn <- ceiling(max(10, n_iter/2)) #happens first and gets rid of noise 
-n_sample <- ceiling(n_iter / n_thin)
-n_chain <- 3
-
-inits <- replicate(n_chain,
-                   list(.RNG.name = "base::Mersenne-Twister",
-                        .RNG.seed = NA,
-                        mean.p = 0.25,
-                        mean.phi = 0.999),
-                   simplify = FALSE)
-
-for (j in 1:n_chain) inits[[j]]$.RNG.seed <- (j - 1) * 10 + 1
-
-
-# run ---------------------------------------------------------------------
-
-post <- runjags::run.jags(model = mcjs$model,
-                          monitor = para,
-                          data = d_jags,
-                          n.chains = n_chain,
-                          inits = inits,
-                          method = "parallel",
-                          burnin = n_burn,
-                          sample = n_sample,
-                          adapt = n_ad,
-                          thin = n_thin,
-                          n.sims = n_chain,
-                          module = "glm") #specific to jags 
-
-MCMCvis::MCMCsummary(post$mcmc) %>% 
-  as_tibble(rownames = "para") %>% 
-  mutate(y = x)
-
+  
+  df_tag <- df_i %>% #was df_tag0
+    #filter(species == sp) %>% 
+    mutate(tag_index = as.numeric(as.factor(tag_id))) %>% 
+    arrange(tag_index, occasion) %>% 
+    relocate(tag_index, occasion)
+  
+  ## interval information
+  intv0 <- df_i %>% #was df_tag0
+    group_by(occasion) %>% 
+    summarize(min_date0 = min(datetime)) %>% 
+    mutate(min_date1 = lead(min_date0),
+           intv = round(min_date1 - min_date0) %>% 
+             as.numeric()) %>% 
+    drop_na(intv) %>% 
+    pull(intv)
+  
+  df_consec <- df_c %>% 
+    #readRDS("data_formatted/data_move.rds") %>% 
+    #filter(species == sp) %>% 
+    mutate(intv = datetime1 - datetime0) %>% 
+    group_by(occasion0) %>% 
+    mutate(intv = ifelse(is.na(intv),
+                         yes = intv0[occasion0],
+                         no = intv),
+           intv = round(intv)) %>% 
+    select(tag_id,
+           occasion = occasion0,
+           intv) %>% 
+    ungroup()
+  
+  ## create vectorized recapture format
+  df_y <- df_tag %>% 
+    pivot_wider(id_cols = c(tag_index, tag_id),
+                names_from = occasion,
+                values_from = section) %>% 
+    pivot_longer(cols = -c(tag_index, tag_id),
+                 names_to = "occasion",
+                 values_to = "y") %>% 
+    mutate(y = ifelse(is.na(y), 0, 1),
+           occasion = as.numeric(occasion)) %>% 
+    arrange(tag_index, occasion) %>% 
+    left_join(df_consec)
+  
+  ## append season column to df_y
+  df_season <- df_tag %>% 
+    mutate(month = format(datetime, "%m") %>% 
+             as.numeric(month),
+           season = ifelse(between(month, 4, 9),
+                           yes = 1,
+                           no = 0)) %>% 
+    distinct(occasion,
+             season) %>% 
+    arrange(occasion)
+  
+  df_y <- left_join(df_y, df_season)
+  
+  # create first capture vector 
+  df_fc <- df_y %>% 
+    group_by(tag_index) %>% 
+    filter(y == 1) %>% 
+    slice(which.min(occasion)) %>% 
+    ungroup() %>% 
+    arrange(tag_index)
+  
+  ## create movement vector
+  df_dist <- df_tag %>% 
+    pivot_wider(id_cols = tag_index,
+                names_from = occasion,
+                values_from = section) %>% 
+    pivot_longer(cols = -tag_index,
+                 names_to = "occasion",
+                 values_to = "section") %>% 
+    mutate(x = 10 * section - 5,
+           occasion = as.numeric(occasion)) %>% 
+    arrange(tag_index, occasion) %>% 
+    drop_na(x)
+  
+  ## binary recapture record
+  list_recap <- with(df_y,
+                     list(Y = y, # capture state  
+                          Id_tag_y = tag_index, # tag id
+                          Id_occ_y = occasion, # occasion
+                          Nind = n_distinct(tag_index), # n unique ind
+                          Nocc = n_distinct(occasion), # n unique occasions
+                          Nobs = nrow(df_y),
+                          Season = season,
+                          L = 430, # total length
+                          Fc = df_fc$occasion # first capture
+                     ))
+  
+  list_intv <- with(df_y %>% drop_na(intv),
+                    list(Intv = intv,
+                         Nint = nrow(df_y %>% drop_na(intv)),
+                         Id_tag_int = tag_index, # tag id
+                         Id_occ_int = occasion))
+  
+  ## movement distance
+  list_move <- with(df_dist,
+                    list(X = x, # distance class
+                         Id_tag_x = tag_index,
+                         Id_occ_x = occasion,
+                         Nx = nrow(df_dist)))
+  
+  
+  d_jags <- c(list_recap, list_intv, list_move)
+  
+  para <- c("sd_x", "mean.p", "mu.p", "alpha", "mean.phi", "sd_phi")
+  
+  # mcmc setup --------------------------------------------------------------
+  
+  ## model file ####
+  mcjs <- runjags::read.jagsfile("code/model_cjs_move.R")
+  
+  ## mcmc setup ####
+  n_ad <- 1000
+  n_iter <- 1.0E+3
+  n_thin <- max(3, ceiling(n_iter / 250)) #happens second want chains to converge 
+  n_burn <- ceiling(max(10, n_iter/2)) #happens first and gets rid of noise 
+  n_sample <- ceiling(n_iter / n_thin)
+  n_chain <- 3
+  
+  inits <- replicate(n_chain,
+                     list(.RNG.name = "base::Mersenne-Twister",
+                          .RNG.seed = NA,
+                          mean.p = 0.25,
+                          mean.phi = 0.999),
+                     simplify = FALSE)
+  
+  for (j in 1:n_chain) inits[[j]]$.RNG.seed <- (j - 1) * 10 + 1
+  
+  
+  # run ---------------------------------------------------------------------
+  
+  post <- runjags::run.jags(model = mcjs$model,
+                            monitor = para,
+                            data = d_jags,
+                            n.chains = n_chain,
+                            inits = inits,
+                            method = "parallel",
+                            burnin = n_burn,
+                            sample = n_sample,
+                            adapt = n_ad,
+                            thin = n_thin,
+                            n.sims = n_chain,
+                            module = "glm") #specific to jags 
+  
+  MCMCvis::MCMCsummary(post$mcmc) %>% 
+    as_tibble(rownames = "para") %>% 
+    mutate(y = x)
+  
 }
 
 bhc <- as.data.frame(as.matrix(list_est_usp[[1]]))
