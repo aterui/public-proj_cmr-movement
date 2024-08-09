360d148287e62dca6236081bc694207fa8c03481
aterui <hanabi0111@gmail.com>
2024-08-09 17:51:20 -0400

    fix: error fix to mutate()

--- code/run_model_cjs_move.R
+++ code/run_model_cjs_move.R
@@ -81,7 +81,7 @@ list_est <- foreach(x = usp) %do% {
   
   ## append season column to df_y
   df_season <- df_tag %>% 
-    mutate(month = format(datetime, "%m"), 
+    mutate(month = format(datetime, "%m") %>%
              as.numeric(month),
            season = ifelse(between(month, 4, 9),
                            yes = 1,
@@ -148,8 +148,7 @@ list_est <- foreach(x = usp) %do% {
             "mu.p",
             "mu.phi",
             "alpha",
-            "mean.phi",
-            "sd_phi")
+            "mean.phi")
   
   # mcmc setup --------------------------------------------------------------
   
@@ -157,8 +156,8 @@ list_est <- foreach(x = usp) %do% {
   mcjs <- runjags::read.jagsfile("code/model_cjs_move.R")
   
   ## mcmc setup ####
-  n_ad <- 500
-  n_iter <- 20000
+  n_ad <- 100
+  n_iter <- 10000
   n_thin <- max(3, ceiling(n_iter / 500)) #happens second want chains to converge 
   n_burn <- ceiling(max(10, n_iter/2)) #happens first and gets rid of noise 
   n_sample <- ceiling(n_iter / n_thin)
